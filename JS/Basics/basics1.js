fd781df4097a1df844c9f6e8ea3fd3e23917931a
Kavyan Nayak <kavyannayak104@gmail.com>
2025-03-31 11:35:23 +0530

    Basic - 1

---
+++ JS/Basics/basics1.js
@@ -0,0 +1,34 @@
+// printf(".....");
+console.log("Hello World");
+console.error("This is an a Error");
+console.warn("This is a Warning !");
+
+// int a = 10;
+// printf("%d",a)
+// printf("Value of a is : %d",a)
+
+var a = 10;
+console.log(a);
+console.log("Value of a is :",a);
+console.log("typeof a :-",typeof a)
+
+
+var x = 89.99
+
+console.log("Value of x =",x);
+console.log("Typeof x = ", typeof x);
+
+var c = "kavyan"
+console.log("Name :", c);
+console.log("Type of c =",typeof c);
+
+
+var isActive = ture;
+console.log("isActive = ",isActive);
+console.log("type of isActive = ",typeof isActive);
+
+
+var v;
+
+console.log("Value of v",v);
+console.log("Type of v =",typeof v);
