d1c2a1e294f75b2b696d867bbc27a64a3c56782f
Kavyan Nayak <kavyannayak104@gmail.com>
2025-05-26 13:14:43 +0530

    Basics Array

--- JS/array/array1.js
+++ JS/array/array1.js
@@ -3,3 +3,26 @@
 var marks = [10, 20, 30, 40, 50];
 
 console.log(marks);
+
+// starting from index ; condition ; increment
+for (let i = 0; i < marks.length; i++) {
+    console.log(marks[i]);
+}
+
+console.log("Using Each Element");
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+// console.log(marks[5]); // undefined
+
+
+var sum = 0;
+for (let i = 0; i < marks.length; i++) {
+
+    sum = sum + marks[i];
+
+}
+
+console.log("Sum of Marks: " + sum);
